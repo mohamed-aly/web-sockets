8995286c1df71041336c0a83429e266d85b5cf4c
mohamed-aly <mali902016@gmail.com>
2022-06-22 13:33:23 +0200

    websocket project

---
+++ src/main/java/com/sockets/demo/websockets/Message.java
@@ -0,0 +1,15 @@
+package com.sockets.demo.websockets;
+
+public class Message {
+
+    private String from;
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+}
